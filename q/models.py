95de4c6ede3d28d6123429484d536d513824c418
MoZol99 <maks.mozolyuk@yandex.ru>
2021-09-27 17:45:33 +0300

    new field

--- q/models.py
+++ q/models.py
@@ -139,6 +139,7 @@ class Group(BaseGroup):
     pass
 
 class Player(BasePlayer):
+    inequality_with_images =models.IntegerField(label='Dont forget about images!!!')
     gender = models.BooleanField(initial=None,
                                  choices=Constants.GENDER_CHOICES,
                                  label=_('Indicate your gender'),
